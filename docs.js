cff2c54ba861e98c49b0788c0da3a8168837be2f
Kamran Ayub <kamran.ayub@gmail.com>
2015-03-29 18:32:06 +0200

    Update docs script

--- docs.js
+++ docs.js
@@ -1,47 +1,27 @@
-var git = require('gift');
 var path = require('path');
-var async = require('async');
 var child_process = require('child_process');
 var rimraf = require('rimraf');
 
 var BUILD_CMD = path.join('..', 'node_modules', '.bin', 'typedoc');
 
-var repo = git(path.join(__dirname, 'Excalibur'));
+console.log("Fetching Excalibur... ");
 
-console.log("Fetching Excalibur... " + repo.path);
+process.stdout.write(child_process.execSync("git checkout master", { cwd: './Excalibur' }));
+process.stdout.write(child_process.execSync("git pull origin master", { cwd: './Excalibur' }));
 
-repo.remote_fetch('origin', function (err) {
+console.log("Removing existing docs...");
 
-	if (err) {
-		console.log(err);
-		return;
-	}
+rimraf.sync('pages/api/edge');
 
-	console.log("Repository fetched");
+console.log("Executing typedoc...");
 
-	console.log("Checkout master...");
-
-	// Generate docs for edge (master)
-	repo.checkout('master', function (err) {
-
-		console.log("Removing existing docs...");
-
-		rimraf.sync('pages/api/edge');
-
-		console.log("Executing typedoc...");
-
-		var typedoc = child_process.execSync(BUILD_CMD + 
-			' -t ES5' +
-			' --name "Excalibur.js Edge API Documentation"' +
-			' --readme none' +
-			' --hideGenerator' +
-			' --mode file' +
-			' --out ../pages/api/edge' +
-			' src/engine', {
-			cwd: './Excalibur'
-		});
-		process.stdout.write(typedoc);
-	});
-
-	
-});
+process.stdout.write(child_process.execSync(BUILD_CMD + 
+	' -t ES5' +
+	' --name "Excalibur.js Edge API Documentation"' +
+	' --readme none' +
+	' --hideGenerator' +
+	' --mode file' +
+	' --out ../pages/api/edge' +
+	' src/engine', {
+	cwd: './Excalibur'
+}));
