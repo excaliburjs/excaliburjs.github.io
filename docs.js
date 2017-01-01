b092b2b9b02a5150272b681f1a5bb4d8b6c840c2
Kamran Ayub <kamran.ayub@gmail.com>
2017-01-01 15:56:32 -0600

    Support modules

--- docs.js
+++ docs.js
@@ -1,14 +1,17 @@
 var path = require('path');
+var fs = require('fs');
 var child_process = require('child_process');
 var rimraf = require('rimraf');
 
 var BUILD_CMD = path.join('..', 'node_modules', '.bin', 'typedoc');
 
-rimraf.sync('Excalibur');
-
 console.log("Fetching Excalibur... ");
 
-child_process.execSync("git clone https://github.com/excaliburjs/Excalibur", {stdio: [0,1,2]});
+if (!fs.existsSync('./Excalibur')) {
+	child_process.execSync("git clone https://github.com/excaliburjs/Excalibur", {stdio: [0,1,2]});
+} else {
+	child_process.execSync("git pull", { cwd: './Excalibur', stdio: [0,1,2]});
+}
 child_process.execSync("git rev-parse HEAD", { cwd: "./Excalibur", stdio: [0,1,2] });
 
 console.log("Removing existing docs...");
@@ -17,25 +20,29 @@ rimraf.sync('pages/api/edge');
 
 console.log("Compiling default template (default)...");
 try {
-	child_process.execSync("npm install", { cwd: "./typedoc-default-themes", stdio: [0,1,2] });
+	if (!fs.existsSync('./typedoc-default-themes/node_modules')) {
+		child_process.execSync("npm install", { cwd: "./typedoc-default-themes", stdio: [0,1,2] });
+	}
 } catch (e) {
 	// fails to execute Linux commands, OK
 }
+
 console.log("Executing typedoc...");
 
 child_process.execSync(BUILD_CMD + 
 	' --target ES5' +
 	' --experimentalDecorators' +
 	' --name "Excalibur.js Edge API Documentation"' +
-	' --readme none' +
-	' --mode file' +	
+	' --readme src/engine/Docs/Index.md' +
+	' --mode modules' +
+	' --module amd' +
     ' --includes src/engine/Docs' +
 	' --out ../pages/api/edge' +
 	' --theme ../typedoc-default-themes/bin/default' +
 	' --hideGenerator' +
+	' --excludePrivate' +
 	' --gaID UA-46390208-1' +
-	' --gaSite excaliburjs.com' +	
-	' --entryPoint ex' +
+	' --gaSite excaliburjs.com' +
 	' src/engine', {
 	cwd: './Excalibur',
 	stdio: [0,1,2]
