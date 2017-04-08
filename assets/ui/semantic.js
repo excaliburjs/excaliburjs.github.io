8090fd8d1f909c61eabd4f826af2e2bb7e783bd5
Travis-CI <travis@excaliburjs.com>
2017-04-08 18:54:25 +0000

    Deploying site for 821507df8517e71d822e230e58fc1051572cfb8d

--- assets/ui/semantic.js
+++ assets/ui/semantic.js
@@ -1,5 +1,5 @@
  /*
- * # Semantic UI - 2.2.10
+ * # Semantic UI - 2.2.7
  * https://github.com/Semantic-Org/Semantic-UI
  * http://www.semantic-ui.com/
  *
@@ -9,7 +9,7 @@
  *
  */
 /*!
- * # Semantic UI 2.2.10 - Site
+ * # Semantic UI 2.2.7 - Site
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -497,7 +497,7 @@ $.extend($.expr[ ":" ], {
 })( jQuery, window, document );
 
 /*!
- * # Semantic UI 2.2.10 - Transition
+ * # Semantic UI 2.2.7 - Transition
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -1593,7 +1593,7 @@ $.fn.transition.settings = {
 })( jQuery, window, document );
 
 /*!
- * # Semantic UI 2.2.10 - State
+ * # Semantic UI 2.2.7 - State
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -2302,7 +2302,7 @@ $.fn.state.settings = {
 })( jQuery, window, document );
 
 /*!
- * # Semantic UI 2.2.10 - Visibility
+ * # Semantic UI 2.2.7 - Visibility
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -2690,11 +2690,7 @@ $.fn.visibility = function(parameters) {
               .attr('src', src)
             ;
             if(settings.transition) {
-              if( $.fn.transition !== undefined) {
-                if($module.hasClass(className.visible)) {
-                  module.debug('Transition already occurred on this image, skipping animation');
-                  return;
-                }
+              if( $.fn.transition !== undefined ) {
                 $module.transition(settings.transition, settings.duration, callback);
               }
               else {
@@ -2725,22 +2721,6 @@ $.fn.visibility = function(parameters) {
               return !(module.cache.element.width === 0 && module.cache.element.offset.top === 0);
             }
             return false;
-          },
-          verticallyScrollableContext: function() {
-            var
-              overflowY = ($context.get(0) !== window)
-                ? $context.css('overflow-y')
-                : false
-            ;
-            return (overflowY == 'auto' || overflowY == 'scroll');
-          },
-          horizontallyScrollableContext: function() {
-            var
-              overflowX = ($context.get(0) !== window)
-                ? $context.css('overflow-x')
-                : false
-            ;
-            return (overflowX == 'auto' || overflowX == 'scroll');
           }
         },
 
@@ -3198,13 +3178,6 @@ $.fn.visibility = function(parameters) {
             element.offset        = $module.offset();
             element.width         = $module.outerWidth();
             element.height        = $module.outerHeight();
-            // compensate for scroll in context
-            if(module.is.verticallyScrollableContext()) {
-              element.offset.top += $context.scrollTop() - $context.offset().top;
-            }
-            if(module.is.horizontallyScrollableContext()) {
-              element.offset.left += $context.scrollLeft - $context.offset().left;
-            }
             // store
             module.cache.element = element;
             return element;
@@ -3228,10 +3201,10 @@ $.fn.visibility = function(parameters) {
             }
 
             // visibility
+            element.topVisible       = (screen.bottom >= element.top);
             element.topPassed        = (screen.top >= element.top);
+            element.bottomVisible    = (screen.bottom >= element.bottom);
             element.bottomPassed     = (screen.top >= element.bottom);
-            element.topVisible       = (screen.bottom >= element.top) && !element.bottomPassed;
-            element.bottomVisible    = (screen.bottom >= element.bottom) && !element.topPassed;
             element.pixelsPassed     = 0;
             element.percentagePassed = 0;
 
@@ -3600,8 +3573,7 @@ $.fn.visibility.settings = {
 
   className: {
     fixed       : 'fixed',
-    placeholder : 'placeholder',
-    visible     : 'visible'
+    placeholder : 'placeholder'
   },
 
   error : {
