d9815e31d157cb0cb945222d1aae211a0ca72a43
Erik Onarheim <erik.onarheim@gmail.com>
2021-04-22 23:00:05 -0500

    docs: Update docs for graphics context and generally improve quality (#62)
    * WIP new graphics docs
    
    * Polish up documentation
    
    * Fix some wrong info

--- src/data/samples.js
+++ src/data/samples.js
@@ -1,28 +1,36 @@
 import sampleExcalibird from '../assets/images/showcase/excalibird.png'
 import sampleShmup from '../assets/images/showcase/shmup.png'
 import sampleBreakout from '../assets/images/showcase/breakout.png'
+import samplePlatformer from '../assets/images/showcase/platformer.gif'
 
 export default [
+  {
+    title: 'Platformer',
+    image: samplePlatformer,
+    description: 'This is a small platforming example.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-platformer',
+    source: 'https://github.com/excaliburjs/sample-platformer',
+  },
   {
     title: 'Excalibird',
     image: sampleExcalibird,
     description:
       'This is a sample clone of the popular mobile game flappy bird.',
-    url: 'http://excaliburjs.com/excalibird/',
+    url: 'https://codesandbox.io/s/github/excaliburjs/excalibird',
     source: 'https://github.com/excaliburjs/excalibird',
   },
   {
     title: "Shoot 'Em Up",
     image: sampleShmup,
     description: "This is an example of how to create a Shoot 'Em Up game",
-    url: 'https://excaliburjs.com/sample-shootemup/',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-shootemup',
     source: 'https://github.com/excaliburjs/sample-shootemup',
   },
   {
     title: 'Breakout',
     image: sampleBreakout,
     description: 'This is a sample brick breaking game.',
-    url: 'http://jsfiddle.net/excaliburjs/s33mLjn3',
-    source: 'https://github.com/excaliburjs/ExcaliburBreakout',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-breakout',
+    source: 'https://github.com/excaliburjs/sample-breakout',
   },
 ]
