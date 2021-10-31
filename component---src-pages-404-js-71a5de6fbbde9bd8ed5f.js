f44be035313bd75aaeefe1c37d74fa893d36e3f1
kamranayub <kamranayub@users.noreply.github.com>
2021-10-31 02:49:41 +0000

    deploy: 8d9118ca6ab5e9f663103d1b93d8e17f6d0f78f7

--- component---src-pages-404-js-71a5de6fbbde9bd8ed5f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-71a5de6fbbde9bd8ed5f.js.map
