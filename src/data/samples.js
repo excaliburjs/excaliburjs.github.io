cc4604a31559fd6eddf9c0524b88169efa9ab460
Erik Onarheim <erik.onarheim@gmail.com>
2023-11-18 09:52:31 -0600

    Update samples

--- src/data/samples.js
+++ src/data/samples.js
@@ -2,14 +2,65 @@ import sampleExcalibird from '../assets/images/showcase/excalibird.png'
 import sampleShmup from '../assets/images/showcase/shmup.png'
 import sampleBreakout from '../assets/images/showcase/breakout.png'
 import samplePlatformer from '../assets/images/showcase/platformer.gif'
+import sampleElectron from '../assets/images/showcase/electron.png'
+import sampleMatterjs from '../assets/images/showcase/matterjs.gif'
+import sampleTiled from '../assets/images/showcase/tiled.gif'
+import sampleGrid from '../assets/images/showcase/grid.gif'
 
 export default [
+  {
+    title: 'Grid Movement',
+    image: sampleGrid,
+    description: 'Example of building grid based movement.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-gird',
+    source: 'https://github.com/excaliburjs/sample-grid',
+  },
+  {
+    title: 'Tiled w/ Parcel',
+    image: sampleTiled,
+    description:
+      'This is a small level with the Tiled Plugin and Parcel bundler.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-tiled-parcel',
+    source: 'https://github.com/excaliburjs/sample-tiled-parcel',
+  },
+  {
+    title: 'Tiled w/ Vite',
+    image: sampleTiled,
+    description:
+      'This is a small level with the Tiled Plugin and Vite bundler.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-tiled-vite',
+    source: 'https://github.com/excaliburjs/sample-tiled-vite',
+  },
+  {
+    title: 'Tiled w/ Webpack',
+    image: sampleTiled,
+    description:
+      'This is a small level with the Tiled Plugin and Webpack bundler.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-tiled-webpack',
+    source: 'https://github.com/excaliburjs/sample-tiled-webpack',
+  },
   {
     title: 'Platformer',
     image: samplePlatformer,
     description: 'This is a small platforming example.',
     url: 'https://codesandbox.io/s/github/excaliburjs/sample-platformer',
     source: 'https://github.com/excaliburjs/sample-platformer',
+  },
+  {
+    title: 'Matter.js',
+    image: sampleMatterjs,
+    description:
+      'This is a small sample showing how to wire in a 3rd party physics instead of using Excalibur.',
+    url: 'https://codesandbox.io/s/github/excaliburjs/sample-matterjs',
+    source: 'https://github.com/excaliburjs/sample-matterjs',
+  },
+  {
+    title: 'Electron Template',
+    image: sampleElectron,
+    description:
+      'This is a template to show Electron integration with Excalibur. Useful for shipping games to desktops or steam!',
+    url: 'https://codesandbox.io/s/github/excaliburjs/template-electron',
+    source: 'https://github.com/excaliburjs/template-electron',
   },
   {
     title: 'Excalibird',
